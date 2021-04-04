2feaf9e37eca46abc64cb04694eaaf269452715e
Eesha Nayak <eeshanayak@utexas.edu>
2021-04-04 12:45:58 -0500

    modified app.py

--- midterm/redis-docker/app.py
+++ midterm/redis-docker/app.py
@@ -11,10 +11,6 @@ import uuid
 rd = redis.StrictRedis(host='redis', port=6379, db=0)
 app = Flask(__name__)
 
-def get_data():
-    userdata = json.loads(rd.get('animals').decode('utf-8'))
-    return userdata
-
 @app.route('/helloworld', methods = ['GET'])
 def hello_world():
     return 'hello world'
@@ -22,37 +18,30 @@ def hello_world():
 @app.route('/update', methods=['GET'])
 def update():
     uuid = request.args.get('uuid')
-    animals = get_data()
-
+  
     head = request.args.get('head')
     body = request.args.get('body')
     arms = request.args.get('arms')
     legs = request.args.get('legs')
     tails = request.args.get('tails')
 
-    for x in animals['animals']:
-        if(x['uuid'] == uuid):          
-            if head is not None:
-                x['head'] = head
-            if body is not None:
-                x['body'] = body
-            if arms is not None:
-                x['arms'] = arms
-            if legs is not None:
-                x['legs'] = legs
-            if head is not None:
-                x['tails'] = tails
-    
+    if head is not None:
+        rd.hset(uuid, 'head', head)
+    if body is not None:
+        rd.hser(uuid, 'body', body)
+    if arms is not None:
+        rd.hset(uuid, 'arms', arms)
+    if legs is not None:
+        rd.hset(uuid, 'legs', legs)
+    if tails is not None:
+        rd.hset(uuid, 'tails', tails)    
 
-    rd.set('animals', json.dumps(animals))
     return str(uuid + 'has been updated')
 
 
 @app.route('/generate', methods=['GET'])
 def generate():
-    animal_dict = {}
-    animal_dict['animals'] = []
-  
+     
     for i in range(20):
         this_animal = {}
         this_animal['head'] = random.choice(['snake', 'bull', 'lion', 'raven', 'bunny'])
@@ -61,45 +50,41 @@ def generate():
         this_animal['legs'] = random.randint(1,4) * 3
         this_animal['tail'] = this_animal['legs'] + this_animal['arms']
         this_animal['created_on'] = str(datetime.datetime.now())
-        this_animal['uuid'] = str(uuid.uuid4())
  
-        animal_dict['animals'].append(this_animal)
+        rd.hmset(str(uuid.uuid4()), this_animal)
   
-    rd.set('animals', json.dumps(animal_dict))
-
     return '20 animals have been generated'
 
 @app.route('/animals', methods=['GET'])
 def get_animals():
-    return json.dumps(get_data())
+    animals = []
+    keys = rd.keys()
+
+    for key in keys:
+        animals.append(rd.hgetall(key))
+    
+    return str(animals)
 
 @app.route('/select', methods=['GET'])
 def get_uuid():
     uuid = request.args.get('uuid')
-    animals = get_data()
-   
-    for x in animals['animals']:
-       if(x['uuid'] == uuid):
-           return x
+    return rd.hgetall(uuid)
 
     return 'No animals with given uuid'
 
 @app.route('/avg_legs', methods=['GET'])
 def get_avg_legs():
-    animals = get_data()    
-
+    keys = rd.keys()
     legs = 0
-    for x in animals['animals']:
-        legs += x['legs']
 
-    return str(legs / len(animals['animals']))
+    for key in keys:
+        legs += int(rd.hget(key, 'legs'))
+
+    return str(legs / rd.dbsize())
 
 @app.route('/total_count', methods=['GET'])
 def get_total():   
-    animals = get_data()
-    total_animals = len(animals['animals'])
-
-    return str(total_animals)
+    return str(rd.dbsize())
 
 @app.route('/dates', methods=['GET'])
 def query_dates():
@@ -109,15 +94,15 @@ def query_dates():
     startdate = datetime.datetime.strptime(start, '%Y-%m-%d_%H:%M:%S.%f')
     enddate = datetime.datetime.strptime(end, '%Y-%m-%d_%H:%M:%S.%f')
 
-    animals = get_data()
-   
     date_query = []
 
-    for x in animals['animals']:
-        created_on = datetime.datetime.strptime(x['created_on'], '%Y-%m-%d %H:%M:%S.%f')
+    keys = rd.keys()
+
+    for key in keys:
+        created_on = datetime.datetime.strptime(rd.hget(key, 'created_on'), '%Y-%m-%d %H:%M:%S.%f')
 
         if(created_on > startdate and created_on < enddate):
-            date_query.append(x)
+            date_query.append(rd.hgetall(key))
 
     return str(date_query)
 
@@ -130,14 +115,12 @@ def delete():
     startdate = datetime.datetime.strptime(start, '%Y-%m-%d_%H:%M:%S.%f')
     enddate = datetime.datetime.strptime(end, '%Y-%m-%d_%H:%M:%S.%f')
 
-    animals = get_data()
-   
-    for x in animals['animals']:
-        created_on = datetime.datetime.strptime(x['created_on'], '%Y-%m-%d %H:%M:%S.%f')
-        if(created_on > startdate or created_on < enddate):
-            animals['animals'].remove(x)
+    keys = rd.keys()   
 
-    rd.set('animals', json.dumps(animals))
+    for key in keys:
+        created_on = datetime.datetime.strptime(rd.hget(key, 'created_on'), '%Y-%m-%d %H:%M:%S.%f')
+        if(created_on > startdate or created_on < enddate):
+            rd.delete(key)
 
     return 'Animals created between ' + start + ' and ' + end + ' have been deleted'
 
@@ -145,10 +128,8 @@ def delete():
 @app.route('/reset', methods=['GET'])
 def reset():
     rd.flushall()
-
     return 'Database is reset'
 
 
 if __name__ == '__main__':
-    rd = redis.StrictRedis(host='redis', port=6379, db=0)
     app.run(debug=True, host='0.0.0.0', port=5000)
